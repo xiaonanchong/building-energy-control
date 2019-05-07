2e7b8fe2a24e82f5b37bdf8d42268f463f49e9a3
xiaonanchong <xnchong@outlook.com>
2019-05-07 18:45:44 +0100

    Update pg-pong.py

--- pg-pong.py
+++ pg-pong.py
@@ -1,6 +1,6 @@
 """ Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
 import numpy as np
-import cPickle as pickle
+import _pickle as pickle
 import gym
 
 # hyperparameters
