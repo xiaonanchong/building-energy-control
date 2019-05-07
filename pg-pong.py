772647ab671f5fab74958f560de6decb52010592
xiaonanchong <xnchong@outlook.com>
2019-05-07 18:51:49 +0100

    Update pg-pong.py

--- pg-pong.py
+++ pg-pong.py
@@ -40,7 +40,7 @@ def discount_rewards(r):
   """ take 1D float array of rewards and compute discounted reward """
   discounted_r = np.zeros_like(r)
   running_add = 0
-  for t in reversed(xrange(0, r.size)):
+  for t in reversed(range(0, r.size)):
     if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
     running_add = running_add * gamma + r[t]
     discounted_r[t] = running_add
