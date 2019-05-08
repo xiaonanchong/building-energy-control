2c4a6e512593b4c2676c49d7ed98f8e31b1b9a41
xiaonanchong <xnchong@outlook.com>
2019-05-08 20:31:41 +0100

    Update policyGradient.py

--- policyGradient.py
+++ policyGradient.py
@@ -7,7 +7,7 @@ output_size = 1
 
 num_trace = 100
 trace_length = 100
-num_iteration = 1000000
+max_iteration = 1000000
 
 ## mlp initialization
 w1 = np.random.randn(hidden_size, input_size) / np.sqrt(input_size)
@@ -15,14 +15,36 @@ b1 = np.random.randn(hidden_size) / np.sqrt(hidden_size)
 w2 = np.random.randn(hidden_size, output_size) / np.sqrt(hidden_size)
 b2 = np.random.randn(output_size)
 
+  
+def mlp_forward(input):
+  h1 = np.dot(w1, input) + b1
+  h1[h1<0] = 0
+  h2 = np.dot(w2, h2) + b2
+  output = 1.0 / (1.0 + np.exp(-h2))
+  return output
+
+def sample_action(output):
+  from random import choices
+  population = [0, 1]
+  weights = [output, 1-output]
+  action = choices(population, weights)
+
+
 def sample_trajectory(current_state, network, environment):
   
   return average_loss
-  
-def mlp_forward(input):
-  
-  return output
 
-def mlp_backward(average_loss):
+def mlp_backprop(average_loss):
   
   
+## policy gradient
+iteration = 0
+while iteration < max_iteration:
+  loss = 0
+  for n in range(num_trace):
+    for t in range(trace_length):
+      
+      
+  
+  mlp_backprop(loss / num_trace)
+  iteration+= 1
